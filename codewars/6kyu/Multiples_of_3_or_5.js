26dbf68b8e611cae26f922972adc305e12e18a13
winterpillar <75409965+winterpillar@users.noreply.github.com>
2021-01-05 15:19:12 +0900

    Update Multiples_of_3_or_5.js

--- codewars/6kyu/Multiples_of_3_or_5.js
+++ codewars/6kyu/Multiples_of_3_or_5.js
@@ -3,6 +3,14 @@
 Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 
 Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is negative, return 0(for languages that do have them)
+
+
+3 또는 5의 배수 인 10 미만의 모든 자연수를 나열하면 3, 5, 6, 9 가 됩니다. 이 배수의 합은 23입니다.
+
+전달된 숫자(number) 미만의 3 또는 5의 배수의 합을 반환하는 solution 함수를 작성하시오. 
+
+참고: 숫자가 3과 5의 배수인 경우 한 번만 계산합니다. 또한 숫자가 음수면 0을 반환합니다.
+
 */
 
 function solution(number){
