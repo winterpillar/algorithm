26d533b9793f6bb440a2f6f5104ae76d42a226eb
winterpillar <75409965+winterpillar@users.noreply.github.com>
2021-01-18 15:53:48 +0900

    Create Valid_Parentheses.js

---
+++ codewars/5kyu/Valid_Parentheses.js
@@ -0,0 +1,20 @@
+/*
+    "()"              =>  true
+    ")(()))"          =>  false
+    "("               =>  false
+    "(())((()())())"  =>  true
+*/
+
+function validParentheses(parens){
+    var length = parens.length;
+    var res = 0;
+
+    for (var i=0; i < length; i++) {
+       parens[i] == ')' ? res-- : res++;
+       if (res < 0) {
+           break;
+       }
+    }
+
+    return !res;
+  }
